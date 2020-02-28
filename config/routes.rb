07d509b1fa7c531aaf052e81f4c0726ea83526df
SPrio <mahantasupriyo@gmail.com>
2020-02-28 23:37:30 +0530

    Add devise and create user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  devise_for :users
   resources :links
+
+  root to: 'links#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
